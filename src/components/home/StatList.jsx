a4698532bdf90544fd07b103d7220b59ac289e63
Kamrul Hasan <63355519+khriad991@users.noreply.github.com>
2023-11-30 02:54:40 +0600

    update statList componet icon

--- src/components/home/StatList.jsx
+++ src/components/home/StatList.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import Image from "next/image";
+import {SlLike} from "react-icons/sl";
+import {GoProjectSymlink} from "react-icons/go";
+import {GiShadowFollower} from "react-icons/gi";
+import {BiSolidCustomize} from "react-icons/bi";
 
 const getData =async () => {
     const res = await fetch(`${process.env.BASEURL}/api/StatList`)
@@ -18,7 +22,7 @@ const StatList =async  () => {
 
                     <div className="w-1/2 lg:w-1/4 centerFlex flex-col bg-white w-[290px] h-[290px] shadow-3xl ">
                         <div className="bg-gradient centerFlex w-[100px] h-[100px] rounded-xl  ">
-                            <Image src="/assets/like.svg" width={40}  height={40} alt=""/>
+                            <GiShadowFollower size={40} />
                         </div>
                         <h2 className="font-semibold text-3xl text-black mt-4 ">{data["followers"]} </h2>
                         <h4 className="font-bold text-base text-black  capitalize">followers </h4>
@@ -26,7 +30,7 @@ const StatList =async  () => {
 
                     <div className="w-1/2 lg:w-1/4 centerFlex flex-col bg-white w-[290px] h-[290px] shadow-3xl ">
                         <div className="bg-gradient centerFlex w-[100px] h-[100px] rounded-xl">
-                            <Image src="/assets/like.svg" width={40}  height={40} alt=""/>
+                            <SlLike size={40} />
                         </div>
                         <h2 className="font-semibold text-3xl text-black mt-4 ">{data["solved"]} </h2>
                         <h4 className="font-bold text-base text-black  capitalize">solved </h4>
@@ -34,7 +38,7 @@ const StatList =async  () => {
 
                     <div className="w-1/2 lg:w-1/4 centerFlex flex-col bg-white w-[290px] h-[290px] shadow-3xl ">
                         <div className="bg-gradient centerFlex w-[100px] h-[100px] rounded-xl  ">
-                            <Image src="/assets/like.svg" width={40}  height={40} alt=""/>
+                            <BiSolidCustomize size={40}  />
                         </div>
                         <h2 className="font-semibold text-3xl text-black mt-4 ">{data["customers"]} </h2>
                         <h4 className="font-bold text-base text-black  capitalize">customers </h4>
@@ -43,7 +47,7 @@ const StatList =async  () => {
 
                     <div className="w-1/2 lg:w-1/4 centerFlex flex-col bg-white w-[290px] h-[290px] shadow-3xl ">
                         <div className="bg-gradient centerFlex w-[100px] h-[100px] rounded-xl  ">
-                            <Image src="/assets/like.svg" width={40}  height={40} alt=""/>
+                            <GoProjectSymlink size={40} />
                         </div>
                         <h2 className="font-semibold text-3xl text-black mt-4 ">{data["projects"]} </h2>
                         <h4 className="font-bold text-base text-black  capitalize">projects </h4>
