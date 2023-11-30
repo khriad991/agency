1b6e171ab47a4d99df6521a6b4270532bc71f708
Kamrul Hasan <63355519+khriad991@users.noreply.github.com>
2023-11-30 13:27:34 +0600

    simple change in login & registe

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -6,6 +6,12 @@ const Page = () => {
     return (
         <>
           <ChildHero/>
+            <div className="bg-green2 w-full py-20 ">
+                <div className="container py-32 flex justify-center items-center">
+
+                    <h1 className="text-green3 text-3xl font-bold text-center capitalize">up Soon</h1>
+                </div>
+            </div>
           <Subscribe/>
         </>
     );
